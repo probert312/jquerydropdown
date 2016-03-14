b3a668e3cd22b0d4a9ef19f69eb0ad1a40b09254
probert312 <probert312@yahoo.com>
2016-03-14 11:09:19 +0200

    main.js

---
+++ js/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	$("html").click(function(){
+  	$("#city_list").hide();
+  });
+
+  $("#city").click(function(){
+  	event.stopPropagation();
+    $(this).select();
+    
+  	$("#city_list").show();
+    
+    $("#city_list li").each(function(){
+      	$(this).show();
+      });
+  });
+  
+  $("#city").keyup(function(event){
+  	strText = $(this).val().toLowerCase();
+    
+    if (strText.length > 0)
+    {
+      $("#city_list li").each(function(){
+        elValueOriginal = $(this).attr("data-city");
+        elValue = elValueOriginal.toLowerCase();
+
+        if (elValue.search(strText) == -1)
+        {        
+          $("#city_list li[data-city='"+elValueOriginal+"']").hide();
+        }
+        else $("#city_list li[data-city='"+elValueOriginal+"']").show();
+      });
+    }
+    else
+    {
+    	$("#city_list li").each(function(){
+      	$(this).show();
+      });
+    }
+  });
+  
+  $("#city_list li").click(function (){
+  	$("#city").val($(this).attr("data-city"));
+  });
+});
